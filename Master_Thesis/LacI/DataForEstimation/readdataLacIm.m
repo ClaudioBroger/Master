f52a2298cde61490133e40674bda90f8e4baf7ab
Claudio Broger <claudiobroger@mbp-von-claudio.home>
2023-01-06 14:51:05 +0100

    Changes per January 6th (LacI model 2 circuits)

--- Master_Thesis/LacI/DataForEstimation/readdataLacIm.m
+++ Master_Thesis/LacI/DataForEstimation/readdataLacIm.m
@@ -9,18 +9,32 @@ data_raw = readtable('/DataForEstimation/Citrine_size_corrected.csv');
 
 data.tdh3 = table2array(data_raw(86,"Mean"));
 data.empty = table2array(data_raw(end, "Mean"));
-data.means = data_raw(1:12,3);
-data.std = data_raw(1:12,6);
+data.means = table2array(data_raw(1:12,3));
+data.std = table2array(data_raw(1:12,6));
 
 modulenames{1} = 'PA_LacI-P4Lacn_cit';
 
-dose = data_raw(1:12,9);
-timepoint = data_raw(1:12,13);
+dose = table2array(data_raw(1:12,9))*1000;
+timepoint = table2array(data_raw(1:12,13));
 
-save('/Users/claudiobroger/Documents/ETH/Master_Thesis/LacI/DataForEstimation/data.mat', 'data', 'modulenames', 'dose', 'timepoint')
+save('/Users/claudiobroger/Documents/ETH/Master/Master_Thesis/LacI/DataForEstimation/data.mat', 'data', 'modulenames', 'dose', 'timepoint')
+
+
+data_W220F.time = 20;
+
+data_W220F.tdh3 = table2array(data_raw(86,"Mean"));
+data_W220F.empty = table2array(data_raw(end, "Mean"));
+data_W220F.means = table2array(data_raw(13:24,3));
+data_W220F.std = table2array(data_raw(13:24,6));
+
+modulenames_W220F{1} = 'PAct1_LacI(W220F)_tCyc1';
+
+dose_W220F = table2array(data_raw(13:24,9))*1000;
+timepoint_W220F = table2array(data_raw(13:24,13));
+
+save('/Users/claudiobroger/Documents/ETH/Master/Master_Thesis/LacI/DataForEstimation/data_W220F.mat', 'data_W220F', 'modulenames_W220F', 'dose_W220F', 'timepoint_W220F')
+
 end
-
-
 
 
 
